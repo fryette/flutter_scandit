ef6b9ffdd190fa6a63167d54b42dd307113a80f4
Kholofelo Moyaba <kmmoyaba@gmail.com>
2019-12-09 04:45:47 +0200

    feat: ios mapping of symbology enums to strings

--- ios/Classes/SwiftFlutterScanditPlugin.swift
+++ ios/Classes/SwiftFlutterScanditPlugin.swift
@@ -9,6 +9,32 @@ public class SwiftFlutterScanditPlugin: NSObject, FlutterPlugin {
     
     private static var SYMBOLOGIES_KEY = "symbologies"
     
+    private static var SYMBOLOGIES_MAP: Dictionary<String, Symbology> = ["EAN13_UPCA":.ean13UPCA,
+                                                                         "UPCE":.upce,
+                                                                         "EAN8":.ean8,
+                                                                         "CODE39":.code39,
+                                                                         "CODE128":.code128,
+                                                                         "CODE11":.code11,
+                                                                         "CODE25":.code25,
+                                                                         "CODABAR":.codabar,
+                                                                         "INTERLEAVED_TWO_OF_FIVE":.interleavedTwoOfFive,
+                                                                         "MSI_PLESSEY":.msiPlessey,
+                                                                         "QR":.qr,
+                                                                         "DATA_MATRIX":.dataMatrix,
+                                                                         "AZTEC":.aztec,
+                                                                         "MAXI_CODE":.maxiCode,
+                                                                         "DOT_CODE":.dotCode,
+                                                                         "KIX":.kix,
+                                                                         "RM4SCC":.rm4scc,
+                                                                         "GS1_DATABAR":.gs1Databar,
+                                                                         "GS1_DATABAR_EXPANDED":.gs1DatabarExpanded,
+                                                                         "GS1_DATABAR_LIMITED":.gs1DatabarLimited,
+                                                                         "PDF417":.pdf417,
+                                                                         "MICRO_PDF417":.microPDF417,
+                                                                         "MICRO_QR":.microQR,
+                                                                         "CODE32":.code32,
+                                                                         "LAPA4SC":.lapa4SC];
+    
     var registrar: FlutterPluginRegistrar!
     
     var result: FlutterResult?
@@ -60,61 +86,17 @@ public class SwiftFlutterScanditPlugin: NSObject, FlutterPlugin {
         
     }
     
-    private func convertToSymbology(name: String) -> Symbology? {
-        switch name {
-        case "EAN13_UPCA":
-            return .ean13UPCA
-        case "UPCE":
-            return .upce
-        case "EAN8":
-            return .ean8
-        case "CODE39":
-            return .code39
-        case "CODE128":
-            return .code128
-        case "CODE11":
-            return .code11
-        case "CODE25":
-            return .code25
-        case "CODABAR":
-            return .codabar
-        case "INTERLEAVED_TWO_OF_FIVE":
-            return .interleavedTwoOfFive
-        case "MSI_PLESSEY":
-            return .msiPlessey
-        case "QR":
-            return .qr
-        case "DATA_MATRIX":
-            return .dataMatrix
-        case "AZTEC":
-            return .aztec
-        case "MAXI_CODE":
-            return .maxiCode
-        case "DOT_CODE":
-            return .dotCode
-        case "KIX":
-            return .kix
-        case "RM4SCC":
-            return .rm4scc
-        case "GS1_DATABAR":
-            return .gs1Databar
-        case "GS1_DATABAR_EXPANDED":
-            return .gs1DatabarExpanded
-        case "GS1_DATABAR_LIMITED":
-            return .gs1DatabarLimited
-        case "PDF417":
-            return .pdf417
-        case "MICRO_PDF417":
-            return .microPDF417
-        case "MICRO_QR":
-            return .microQR
-        case "CODE32":
-            return .code32
-        case "LAPA4SC":
-            return .lapa4SC
-        default:
-            return nil
+    private func convertSymbologyToString(symbology: Symbology) -> String? {
+        if(SwiftFlutterScanditPlugin.SYMBOLOGIES_MAP.values.contains(symbology)){
+            if let match = SwiftFlutterScanditPlugin.SYMBOLOGIES_MAP.first(where: {$1 == symbology}) {
+                return match.key
+            }
         }
+        return symbology.description;
+    }
+    
+    private func convertToSymbology(name: String) -> Symbology? {
+        return SwiftFlutterScanditPlugin.SYMBOLOGIES_MAP[name];
     }
     
 }
@@ -122,9 +104,9 @@ public class SwiftFlutterScanditPlugin: NSObject, FlutterPlugin {
 
 
 extension SwiftFlutterScanditPlugin: BarcodeScannerDelegate {
-    func didScanBarcodeWithResult(data: String, symbology: String) {
+    func didScanBarcodeWithResult(data: String, symbology: Symbology) {
         if let channelResult = result {
-            channelResult(["data": data,"symbology": symbology])
+            channelResult(["data": data,"symbology": convertSymbologyToString(symbology:symbology)])
         }
     }
     
