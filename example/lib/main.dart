82778f22278ae8efd63cd113fdd0ab3d3f5dc1dc
Kholofelo Moyaba <kmmoyaba@gmail.com>
2020-03-19 16:51:32 +0200

    feat: use update plugin in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,50 +1,80 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_scandit/flutter_scandit.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  BarcodeResult barcode;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Scandit example app'),
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () async {
-                  var result = await FlutterScandit(symbologies: [
+      home: BarcodeScanPage(),
+    );
+  }
+}
+
+class BarcodeScanPage extends StatefulWidget {
+  @override
+  _BarcodeScanPageState createState() => _BarcodeScanPageState();
+}
+
+class _BarcodeScanPageState extends State<BarcodeScanPage> {
+  BarcodeResult barcode;
+
+  Future<void> _showError(BuildContext context, String errorMessage) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Barcode scan error'),
+          content: Text(errorMessage ?? "Unknown error"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Scandit example app'),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () async {
+                try {
+                  BarcodeResult result = await FlutterScandit(symbologies: [
                     Symbology.EAN13_UPCA,
                     Symbology.CODE128
-                  ], licenseKey: "-- ENTER YOUR SCANDIT LICENSE KEY HERE -")
+                  ], licenseKey: "-- ENTER YOUR SCANDIT LICENSE KEY HERE --")
                       .scanBarcode();
                   setState(() {
                     barcode = result;
                   });
-                },
-                child: Text('SCAN'),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              barcode != null
-                  ? Text('${barcode.data} ${barcode.symbology}')
-                  : Text('please scan a barcode...'),
-            ],
-          ),
+                } on BarcodeScanException catch (e) {
+                  _showError(context, e.toString());
+                }
+              },
+              child: Text('SCAN'),
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            barcode != null
+                ? Text('${barcode.data} ${barcode.symbology}')
+                : Text('please scan a barcode...'),
+          ],
         ),
       ),
     );
